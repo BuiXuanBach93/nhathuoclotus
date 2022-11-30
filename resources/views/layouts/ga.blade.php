91324699d94a21cdb2d2756cb07af4c257e24697
DESKTOP-RHBHBUL\DonBach <donbach.it@gmail.com>
2022-11-30 22:53:14 +0700

    update google analytics

--- resources/views/layouts/ga.blade.php
+++ resources/views/layouts/ga.blade.php
@@ -1,8 +1,8 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5XQJYB1BMK"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D30K6CW03V"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-5XQJYB1BMK');
+  gtag('config', 'G-D30K6CW03V');
 </script>
